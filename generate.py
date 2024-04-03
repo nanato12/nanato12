5df8dd2be0736878d37a0a2764cc4edb64d2b4a2
nanato12 <admin@okj.info>
2024-04-03 15:31:19 +0900

    add: generate.py

---
+++ generate.py
@@ -0,0 +1,19 @@
+from mdutils.mdutils import MdUtils
+
+from generate_lib.models.skill import Skill
+
+TARGET_MD_FILE_PATH = "README.md"
+
+m = MdUtils(TARGET_MD_FILE_PATH, title="")
+m.new_header(level=1, title="Hi 👋, I'm nanato12")
+
+m.new_header(level=2, title="Skills (Order by years of experience)")
+
+for k, skills in Skill.from_json_file("./data/skills.json").items():
+    m.new_header(level=3, title=k)
+    for skill in skills:
+        m.new_line(skill.html_tag)
+    m.new_line()
+
+with open(TARGET_MD_FILE_PATH, "wt") as f:
+    f.write(m.get_md_text().strip() + "\n")
