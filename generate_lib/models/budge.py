c203ee85c5a88dbfd0eaf714540df39512ed530c
nanato12 <admin@okj.info>
2024-04-05 23:29:10 +0900

    add: Budge model

---
+++ generate_lib/models/budge.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from mdutils.mdutils import MdUtils
+
+from generate_lib.models import BaseModel
+
+
+@dataclass
+class Budge(BaseModel):
+    alt: str
+    icon_url: str
+    destination: str
+
+    @property
+    def markdown(self) -> str:
+        m = MdUtils("")
+
+        return m.new_inline_link(
+            self.destination, m.new_inline_image(self.alt, self.icon_url)
+        )
